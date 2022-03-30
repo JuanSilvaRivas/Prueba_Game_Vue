923907ac960100cad6cbb64030a97b5eeefb5755
JuanSilvaRivas <juansilvarivas@gmail.com>
2022-03-30 17:54:04 -0300

    Add files via upload

---
+++ Prueba_Games_Opinion_JS/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="">
+                    <i class="bi bi-headset"> Games Opinion</i>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ms-auto">
+                        <router-link class="nav-link" to="/"><i class="bi bi-house-fill"></i> Home</router-link>
+                        <router-link class="nav-link" to="/opiniones"><i class="bi bi-chat-square-text-fill"></i> Opiniones</router-link>
+                        <router-link class="nav-link" to="/administracion"><i class="bi bi-people-fill"></i> Administración</router-link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {};
+</script>
+
+<style>
+</style>
---
+++ Prueba_Games_Opinion_JS/src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Administracion from '../views/Administracion.vue'
+import Editar from '../views/Editar.vue'
+import Navbar from '../components/Navbar.vue'
+import Opiniones from '../views/Opiniones.vue'
+Vue.use(VueRouter)
+
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/administracion",
+    name: "Administracion",
+    component: Administracion
+  },
+  {
+    path: "/editar/:id",
+    name: "Editar",
+    props: true,
+    component: Editar
+  },
+  {
+    path: "/navbar",
+    name: "Navbar",
+    component: Navbar
+  },
+  {
+    path: "/Opiniones",
+    name: "Opiniones",
+    component: Opiniones
+  },
+  {
+    path: "/404",
+    name: "Error",
+    component: ()=>import("../views/Error.vue")
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "404"
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
